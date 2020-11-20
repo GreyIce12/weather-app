66e8a9de72ccefe52a7d87db09185fbcce63e677
Grey Ice <47397208+GreyIce12@users.noreply.github.com>
2020-11-20 16:22:31 -0500

    Add files via upload

--- weather app/weatherApi.js
+++ weather app/weatherApi.js
@@ -1,19 +1,20 @@
 const apiKey = "u9R8YkVB1p7VLwdiRx6BLxAirzNGUK47";
 
 const getCityCondition = async (location) =>{
-    
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+
+        const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
+
     const query = `${location}?apikey=${apiKey}`;
 
     const response = await fetch(base + query);
     const data = await response.json();
     return (data[0]);
-}
+};
 
 
 const getCity = async (city)=>{
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${apiKey}&q=${city}`;
     
     const response = await fetch(base + query);  
