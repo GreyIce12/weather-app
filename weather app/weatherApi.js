8e8350292ceafabf68d05b2c19e8fe5dbc2e06bd
Grey Ice <47397208+GreyIce12@users.noreply.github.com>
2020-11-20 19:34:29 -0500

    Update weatherApi.js

--- weather app/weatherApi.js
+++ weather app/weatherApi.js
@@ -2,7 +2,7 @@ const apiKey = "KKnZCJxXNxvnZdTRxYB9FPpfNWn9jkZF";
 
 const getCityCondition = async (location) =>{
 
-        const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
 
     const query = `${location}?apikey=${apiKey}`;
 
@@ -14,7 +14,7 @@ const getCityCondition = async (location) =>{
 
 const getCity = async (city)=>{
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search;
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search;
     const query = `?apikey=${apiKey}&q=${city}`;
     
     const response = await fetch(base + query);  
